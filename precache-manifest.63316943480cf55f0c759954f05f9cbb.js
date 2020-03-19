e5ed1da0b63d76c4aaa03db74698cbeeeb16169f
dkwilson <general@ytechnology.net>
2020-03-18 22:09:03 -0400

    Updates

---
+++ precache-manifest.63316943480cf55f0c759954f05f9cbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d9767918fb9c8e03580f87137cb18f",
+    "url": "/react_todo/index.html"
+  },
+  {
+    "revision": "5eefe036f6b3c758845a",
+    "url": "/react_todo/static/css/main.e3ca50a0.chunk.css"
+  },
+  {
+    "revision": "c89b3af5c5da56c06aa3",
+    "url": "/react_todo/static/js/2.2f0a3d38.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react_todo/static/js/2.2f0a3d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eefe036f6b3c758845a",
+    "url": "/react_todo/static/js/main.530c8557.chunk.js"
+  },
+  {
+    "revision": "a9d25ad046d3f74d8823",
+    "url": "/react_todo/static/js/runtime-main.141c23ff.js"
+  }
+]);
